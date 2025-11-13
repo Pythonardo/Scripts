5620f14cb083bd799df26b8c36dcf28a49fe83c2
Pythonardo <mgh.python@gmail.com>
2025-11-13 12:03:01 -0700

    Delete OutlookCleanAutoComplete.ps1

--- OutlookCleanAutoComplete.ps1
+++
@@ -1,7 +0,0 @@
-﻿ If(Test-Path -Path 'C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE') {
-            Start-Process -FilePath 'C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE' -ArgumentList '/cleanautocompletecache','/recycle'
-        }
-
-If(Test-Path -Path 'C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE') {
-            Start-Process -FilePath 'C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE' -ArgumentList '/cleanautocompletecache','/recycle'
-        }
