ddaf2e99945a605f3b0d7f5e72e22cfc6ab837de
Pythonardo <mgh.python@gmail.com>
2025-11-13 12:06:32 -0700

    Delete StartupSparkScheduledTask.ps1

--- StartupSparkScheduledTask.ps1
+++
@@ -1,4 +0,0 @@
-﻿$Trigger= New-ScheduledTaskTrigger -At 10:00am -Daily
-$User= "NT AUTHORITY\SYSTEM"
-$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "\\fs1\Utility\IT\Software\Scripts\StartupSpark.ps1"
-Register-ScheduledTask -TaskName "StartupSparkScript" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force
