c6f38d6e259d4002a0acee131f11d88edbb834d3
Pythonardo <mgh.python@gmail.com>
2025-11-13 12:07:27 -0700

    Delete disableNetbios.ps1

--- disableNetbios.ps1
+++
@@ -1,2 +0,0 @@
-﻿$regkey = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
-Get-ChildItem $regkey |foreach { Set-ItemProperty -Path "$regkey\$($_.pschildname)" -Name NetbiosOptions -Value 2 -Verbose}
